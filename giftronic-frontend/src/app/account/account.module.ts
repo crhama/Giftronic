9034d8ce8001dd9abc10bb2184160d5137bcf722
Rhamazany <charles.rhamazany@accenturefederal.com>
2018-03-17 23:55:24 -0400

    Added account and console modules and related files

---
+++ giftronic-frontend/src/app/account/account.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ])
+  ],
+  declarations: [LoginComponent, RegisterComponent]
+})
+export class AccountModule { }
--- giftronic-frontend/src/app/app.module.ts
+++ giftronic-frontend/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ConsoleModule } from './console/console.module';
 
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AccountModule } from './account/account.module';
+
 
 
 @NgModule({
@@ -16,7 +19,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: WelcomeComponent }
-    ])
+    ]),
+    ConsoleModule,
+    AccountModule
   ],
   providers: [],
   bootstrap: [AppComponent]
